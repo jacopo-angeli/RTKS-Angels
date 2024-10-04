50e1a00e520833df90e181d276683aa82bf06f9c
jacopo-angeli <jacopo.angeli00@gmail.com>
2024-10-04 21:25:05 +0200

    Changed OS

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,38 @@
-#![no_std]
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_main]
-
-#![allow(unused)]
+#![no_std]
 
 use panic_halt as _;
 
-use cortex_m_rt::entry;
-use stm32f4xx_hal::gpio::GpioExt;
-use stm32f4xx_hal as f4;
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+mod app {
 
-use bern_kernel::exec::process::Process;
-use bern_kernel::exec::runnable::Priority;
-use bern_kernel::exec::thread::Thread;
-use bern_kernel::stack::Stack;
-use bern_kernel::sleep;
-use bern_kernel::units::frequency::ExtMilliHertz;
+    use defmt_rtt as _;
 
-use rtt_target::{rtt_init_print, rprintln};
+    // Resources shared between tasks
+    #[shared]
+    struct Shared {}
 
-static PROC: &Process = bern_kernel::new_process!(my_process1, 8192);
+    // Local resources to specific tasks (cannot be shared)
+    #[local]
+    struct Local {}
 
+    #[init]
+    fn init(_: init::Context) -> (Shared, Local) {
+        (
+            
+            // Initialization of shared resources
+            Shared {},
+            // Initialization of task local resources
+            Local {},
+        )
+    }
 
-mod regular_produces;
-
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    let dp = f4::pac::Peripherals::take().expect("Peripherals cannot be accessed");
-
-    let gpio_g = dp.GPIOG.split();
-
-    let mut green_led = gpio_g.pg13.into_push_pull_output();
-    let mut blue_led = gpio_g.pg14.into_push_pull_output();
-
-    bern_kernel::kernel::init();
-    bern_kernel::time::set_tick_frequency(2.kHz(), 180.MHz());
-
-    PROC.init(move |c| {
-        // Regular producer
-        regular_produces::spawn_green_led_thread(blue_led, c);
-        
-    }).unwrap();
-
-    bern_kernel::start()
+    // Background task, runs whenever no other tasks are running
+    #[idle]
+    fn idle(mut ctx: idle::Context) -> ! {
+        loop {}
+    }
+    
 }
